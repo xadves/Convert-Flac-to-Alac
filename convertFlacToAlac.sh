e2d0caa72cd4df037ff79c5cf58ca4ca5c67e377
Samuel Robinson <sro@mailfires.com>
2024-12-14 15:17:59 -0500

    Updating with TODO and OptArgs

--- convertFlacToAlac.sh
+++ convertFlacToAlac.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-musicDir=.
-convDir=./converted-music
+# Default Tarring Final Converted
+finalTar=false
 
-if [ ! -z "$1" ] ; then
-	echo "[*] Setting search dir to $musicDir"
-	musicDir=$1
-fi
+# Default Flac Directory
+musicDir=.
+
+# TODO Check if uuidgen is avail
+uuid=$(uuidgen)
+
+# Default Final Directory
+convDir=./$uuid
+
+Help() {
+    echo "This is the help menu"
+    echo "Syntax: convertFlacToAlac.sh [-h|d|o|t]"
+    echo "h     Display this menu"
+    echo "d     Set Flac Directory to Search (Default: $musicDir)"
+    echo "o     Set Output Directory (Default unique ID)"
+    echo "t     Tar the Output directory (Default $finalTar)"
+}
+
+while getopts ":hd:o:t:" option; do
+    case $option in
+        h)
+            Help
+            exit;;
+        d)
+            musicDir=$OPTARG;;
+        o)
+            convDir=$OPTARG;;
+        t)
+            finalTar=true
+        /?)
+            echo "[!] Invalid Option"
+            exit;;
+    esac
+done
 
 if [ ! -d "$musicDir" ] ; then
 	echo "[!] $musicDir does not exist!!!"
@@ -18,9 +48,12 @@ if [ ! -d "$convDir" ] ; then
 	mkdir -p $convDir
 fi
 
+# TODO Search case insensitive flac
+# TODO Check File Collision
 echo "[*] Looking for Flac files"
-find $musicDir -path "$convDir" -prune -o -name "*.flac" -exec ls {} \; -exec cp {} $convDir/. \;
+find "$musicDir" -path "$convDir" -prune -o -name "*.flac" -exec ls {} \; -exec cp {} $convDir/. \;
 
+# TODO This number is off by one because header
 fileCount=$(ls -l "$convDir" | wc -l)
 
 echo "[*] Converting $fileCount files"
@@ -29,8 +62,12 @@ for i in $convDir/*
 do
 	newFile=$(echo $i | sed 's/flac/m4a/g')
 	echo "[*] Converting $i"
+    # TODO Check if ffmpeg is avail
 	ffmpeg -v 0 -i "$i" -vcodec copy -acodec alac "$newFile"
 	rm -f "$i"
 done
 
-tar czf ./music.tar.gz "$convDir"
+if [ "$finalTar" = true ] ; then
+    fileName=$(uuidgen)
+    tar czf ./$fileName.tar.gz "$convDir"
+fi
